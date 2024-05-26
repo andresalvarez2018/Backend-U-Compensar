139f8a9ea8e3b4aa1c5a2ac488af1bdf27964de0
andres alvarez <andres.alvarez@esinergia.co>
2024-05-25 19:39:37 -0500

    api y campos nuevos

---
+++ web/modules/custom/apis_app/src/Plugin/rest/resource/CrearUserResource.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\apis_app\Plugin\rest\resource;
+
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\user\Entity\User;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Represents Crear User records as resources.
+ *
+ * @RestResource (
+ *   id = "apis_app_crear_user",
+ *   label = @Translation("Crear User"),
+ *   uri_paths = {
+ *     "canonical" = "/api/apis-app-crear-user/{id}",
+ *     "create" = "/api/apis-app-crear-user"
+ *   }
+ * )
+ *
+ * @DCG
+ * The plugin exposes key-value records as REST resources. In order to enable it
+ * import the resource configuration into active configuration storage. An
+ * example of such configuration can be located in the following file:
+ * core/modules/rest/config/optional/rest.resource.entity.node.yml.
+ * Alternatively you can enable it through admin interface provider by REST UI
+ * module.
+ * @see https://www.drupal.org/project/restui
+ *
+ * @DCG
+ * Notice that this plugin does not provide any validation for the data.
+ * Consider creating custom normalizer to validate and normalize the incoming
+ * data. It can be enabled in the plugin definition as follows.
+ * @code
+ *   serialization_class = "Drupal\foo\MyDataStructure",
+ * @endcode
+ *
+ * @DCG
+ * For entities, it is recommended to use REST resource plugin provided by
+ * Drupal core.
+ * @see \Drupal\rest\Plugin\rest\resource\EntityResource
+ */
+class CrearUserResource extends ResourceBase {
+
+  /**
+   * The key-value storage.
+   *
+   * @var \Drupal\Core\KeyValueStore\KeyValueStoreInterface
+   */
+  protected $storage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    KeyValueFactoryInterface $keyValueFactory,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger, $keyValueFactory);
+    $this->storage = $keyValueFactory->get('apis_app_crear_user');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('keyvalue')
+    );
+  }
+
+  /**
+   * Responds to POST requests and saves the new record.
+   *
+   * @param array $data
+   *   Data to write into the database.
+   *
+   * @return \Drupal\rest\ModifiedResourceResponse
+   *   The HTTP response object.
+   */
+  public function post(array $data) {
+    $user = User::create();
+    $user->setUsername($data['username']);
+    $user->setPassword($data['password']);
+    $user->setEmail($data['username'] . '@example.com');
+    $user->set("field_nombre", $data['nombre']);
+    $user->set("field_apellidos", $data['apellidos']);
+    $user->activate();
+    $user->save();
+    return new ModifiedResourceResponse($data, 201);
+  }
+
+}
